0376009ff7b5e766edff441a2de3eb6c12929393
Craig <craig@goodgriefgames.com>
2023-05-25 22:31:19 -0700

    trying something different

--- main.go
+++ main.go
@@ -5,18 +5,31 @@ import (
 	"io/fs"
 	"os"
 	"path/filepath"
+	"strconv"
+	"strings"
 )
 
 var (
-	prefix     = "├── "
-	indent     = "│   "
-	lastPrefix = "└── "
-	lastIndent = "    "
+	prefix       = "├── "
+	indent       = "│   "
+	lastPrefix   = "└── "
+	lastIndent   = "    "
+	defaultDepth = 3
 )
 
-func printDir(path string, node fs.DirEntry, prefix string) error {
-	fmt.Println(prefix + node.Name())
-	if node.IsDir() {
+func printDir(path string, node fs.DirEntry, prefix string, depth, maxDepth int) error {
+	// Skip hidden files or directories
+	if strings.HasPrefix(node.Name(), ".") {
+		return nil
+	}
+
+	// Print only directories and .fish files
+	if node.IsDir() || filepath.Ext(node.Name()) == ".fish" {
+		fmt.Println(prefix + node.Name())
+	}
+
+	// If it's a directory and we haven't reached max depth, recurse further
+	if node.IsDir() && depth < maxDepth {
 		newPath := filepath.Join(path, node.Name())
 		dirEntries, err := os.ReadDir(newPath)
 		if err != nil {
@@ -25,22 +38,20 @@ func printDir(path string, node fs.DirEntry, prefix string) error {
 
 		for i, entry := range dirEntries {
 			isLast := i == len(dirEntries)-1
-			if entry.IsDir() || filepath.Ext(entry.Name()) == ".fish" {
-				newPrefix := indent
-				if isLast {
-					newPrefix = lastIndent
-				}
-				entryPrefix := prefix + newPrefix
-				newPrefix = prefix + newPrefix
-				if isLast {
-					entryPrefix = prefix + lastPrefix
-				} else {
-					entryPrefix = prefix + prefix
-				}
-				err := printDir(newPath, entry, entryPrefix)
-				if err != nil {
-					return err
-				}
+			newPrefix := indent
+			if isLast {
+				newPrefix = lastIndent
+			}
+			entryPrefix := prefix + newPrefix
+			newPrefix = prefix + newPrefix
+			if isLast {
+				entryPrefix = prefix + lastPrefix
+			} else {
+				entryPrefix = prefix + prefix
+			}
+			err := printDir(newPath, entry, entryPrefix, depth+1, maxDepth)
+			if err != nil {
+				return err
 			}
 		}
 	}
@@ -49,11 +60,20 @@ func printDir(path string, node fs.DirEntry, prefix string) error {
 
 func main() {
 	args := os.Args[1:]
-	if len(args) != 1 {
+	if len(args) < 1 {
 		fmt.Println("Provide directory to diagram")
 		os.Exit(1)
 	}
 	root := args[0]
+	maxDepth := defaultDepth
+	if len(args) > 1 {
+		var err error
+		maxDepth, err = strconv.Atoi(args[1])
+		if err != nil {
+			fmt.Println("Invalid maximum depth:", err)
+			os.Exit(1)
+		}
+	}
 	dirEntries, err := os.ReadDir(root)
 	if err != nil {
 		fmt.Println("Error reading directory:", err)
@@ -62,16 +82,14 @@ func main() {
 	fmt.Println(root)
 	for i, entry := range dirEntries {
 		isLast := i == len(dirEntries)-1
-		if entry.IsDir() || filepath.Ext(entry.Name()) == ".fish" {
-			prefix := prefix
-			if isLast {
-				prefix = lastPrefix
-			}
-			err := printDir(root, entry, prefix)
-			if err != nil {
-				fmt.Println("Error printing directory:", err)
-				os.Exit(1)
-			}
+		prefix := prefix
+		if isLast {
+			prefix = lastPrefix
+		}
+		err := printDir(root, entry, prefix, 1, maxDepth)
+		if err != nil {
+			fmt.Println("Error printing directory:", err)
+			os.Exit(1)
 		}
 	}
 	fmt.Println("Diagram generation completed.")
